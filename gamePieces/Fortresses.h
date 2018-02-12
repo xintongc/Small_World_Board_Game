9f6f269491288a66b353dfca4cd98c0a083f5abf
royanz <tojanet.zhao@gmail.com>
2018-02-12 17:58:49 -0500

    add game pieces class

---
+++ gamePieces/Fortresses.h
@@ -0,0 +1,14 @@
+//
+// Created by yanra on 2018-02-12.
+//
+
+#ifndef SMALL_WORLD_FORTRESSES_H
+#define SMALL_WORLD_FORTRESSES_H
+
+
+class Fortresses {
+
+};
+
+
+#endif //SMALL_WORLD_FORTRESSES_H
---
+++ gamePieces/Heroes.h
@@ -0,0 +1,14 @@
+//
+// Created by yanra on 2018-02-12.
+//
+
+#ifndef SMALL_WORLD_HEROES_H
+#define SMALL_WORLD_HEROES_H
+
+
+class Heroes {
+
+};
+
+
+#endif //SMALL_WORLD_HEROES_H
