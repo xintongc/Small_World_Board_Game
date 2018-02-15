3d63d8abdc951ebc4a46fa45e0f1f0da94ac42aa
royanz <36062607+royanz@users.noreply.github.com>
2018-02-15 12:08:48 -0500

    Update main.cpp
    add test

--- main.cpp
+++ main.cpp
@@ -29,11 +29,25 @@ int main() {
 //
 //    map.display();
 //
-//
+    //part 3
 //    Dice dice;
 //    dice.rollingDice();     //one can request a dice being rolled
 //    cout<<"The dice is being rolled. The number is: "<<dice.getDiceNum()<<endl;   // the container returns the right number of values
 
+    //part 4
+//    Player player1;
+//    cout<<"Player1 rolling the dice is "<<player1.reinforcementDie()<<endl;
+//    cout<<"Players's coins are "<<player1.getCoins()<<endl;
+//    player1.conquers();
+
+    //part 5
+//    Amazons amazons1;
+//    cout<<"amazons1's tokens are "<<amazons1.getTokens()<<endl;
+//
+//    Flying flying;
+//    cout<<"flying's tokens are "<<flying.getTokens()<<endl;
+//
+//    Mountains mountains;
 
 
     return 0;
