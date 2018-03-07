85721650e17f30e26c6087e746c86c1ecf032a28
zncu <xintongc@gmail.com>
2018-03-06 21:33:34 -0500

    update Main and Player

--- main.cpp
+++ main.cpp
@@ -2,37 +2,16 @@
 #include"MapLoader.h"
 #include "Map.h"
 #include "dice.h"
+#include "Game.h"
 
 using namespace std;
 
 
 int main() {
 
-
-    MapLoader mapLoader;
-    mapLoader.openFile("/Users/zncu/CLionProjects/small world/twoPlayer.map");
-    mapLoader.readFile();
-    mapLoader.closeInput();
-//    mapLoader.displayMap();
-    mapLoader.buildMap();
+    Game game;
 
 
-
-
-
-//    Map map = Map(24);
-//    map.addEdge(1,2);
-//    map.addEdge(1,6);
-//    map.addEdge(2,3);
-//    map.addEdge(2,6);
-//    map.addEdge(2,7);
-//
-//    map.display();
-//
-//
-//    Dice dice;
-//    dice.rollingDice();     //one can request a dice being rolled
-//    cout<<"The dice is being rolled. The number is: "<<dice.getDiceNum()<<endl;   // the container returns the right number of values
 
 
 
