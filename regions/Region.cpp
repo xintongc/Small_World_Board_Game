41d4ed7c7079ef1a54d9ad95c057850758371144
royanz <tojanet.zhao@gmail.com>
2018-03-11 16:50:19 -0400

    update

--- regions/Region.cpp
+++ regions/Region.cpp
@@ -85,9 +85,17 @@ void Region::displayGamePieces(){
 }
 
 void Region::info() {
-    cout<< "Region " << id << ": " << "Owmer:" << owner  <<" Population:" << population << " Landform:"
-        << landform << " LandMark:" << landMark << " Border:" << isBorder()
-        << " LostTribes:" << isLostTribes() << " ";
+    if(id<10){
+        cout<< "Region 0" << id << ": " << "Owmer:" << owner  <<" Population:" << population << " Landform:"
+            << landform << " LandMark:" << landMark << " Border:" << isBorder()
+            << " LostTribes:" << isLostTribes() << " ";
+    }
+    else{
+        cout<< "Region " << id << ": " << "Owmer:" << owner  <<" Population:" << population << " Landform:"
+            << landform << " LandMark:" << landMark << " Border:" << isBorder()
+            << " LostTribes:" << isLostTribes() << " ";
+    }
+
     displayGamePieces();
 }
 
