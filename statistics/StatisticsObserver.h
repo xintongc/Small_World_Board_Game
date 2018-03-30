11f2b0401afac560653c5e849dd278c20b95f533
zncu <xintongc@gmail.com>
2018-03-29 20:52:43 -0400

    add statistics

---
+++ statistics/StatisticsObserver.h
@@ -0,0 +1,16 @@
+//
+// Created by xintong cheng on 2018-03-29.
+//
+
+#ifndef SMALL_WORLD_STATISTICSOBSERVER_H
+#define SMALL_WORLD_STATISTICSOBSERVER_H
+
+
+#include "../DesignPattern/Observer.h"
+
+class StatisticsObserver : public Observer {
+
+};
+
+
+#endif //SMALL_WORLD_STATISTICSOBSERVER_H
