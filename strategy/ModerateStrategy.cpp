70bdf93e7cc24e990eb82e6594eff8446b7b978e
zncu <xintongc@gmail.com>
2018-04-02 17:48:34 -0400

    MStrategy changed

--- strategy/ModerateStrategy.cpp
+++ strategy/ModerateStrategy.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include "ModerateStrategy.h"
+#include "../regions/MapRegions.h"
 
 ModerateStrategy::ModerateStrategy() {}
 
@@ -18,11 +19,17 @@ void ModerateStrategy::firstConquestByStrategy(Player* player,int i) {
 }
 
 void ModerateStrategy::picks_raceByStrategy(Player* player, ComboList &combo) {
+    Game* game = Game::getGame();
+    if(game->getRound()==3 && player->getDeclineRace().getType()=="Ghouls"){
+        player->setTotalTokens(0);
+    }
     player->picks_race(combo);
 }
 
 void ModerateStrategy::conquersByStrategy(Player* player, int playerNum) {
     player->conquers(playerNum);
+
+
 }
 
 void ModerateStrategy::declineComboByStrategy(Player* player,ComboList &combo) {
@@ -34,14 +41,27 @@ void ModerateStrategy::declineComboByStrategy(Player* player,ComboList &combo) {
         combo.powerVector.push_back(player->getDeclinePower());
         player->setHaveDeclineCombo(false);
     }
-    player->reduceTokensToOneInDecline();
+
     player->setDeclineRace(player->getActiveRace());
     player->setDeclinePower(player->getActivePower());
     player->setHaveDeclineCombo(true);
     player->setHaveActiveCombo(false);
-    player->currentStates();
+
+    if(game->getRound()==2 && player->getDeclineRace().getType()=="Ghouls"){
+        player->reduceTokensToOneInActiveAndResetToken();
+        player->currentStates();
+        int playerNum = game->Players.size();
+        conquersByStrategy(player, playerNum);
+    }
+    else{
+        player->reduceTokensToOneInDecline();
+        player->currentStates();
+    }
+
+
 }
 
 void ModerateStrategy::redeployTokensByStrategy(Player * player) {
     player->redeployTokens();
 }
+
