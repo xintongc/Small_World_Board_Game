d8bc87b10aa05e03a5580fdef9f1922d7201e667
zncu <xintongc@gmail.com>
2018-04-03 22:34:14 -0400

    change main

--- main.cpp
+++ main.cpp
@@ -8,14 +8,15 @@ using namespace std;
 
 int main() {
 
+    //playing game (will show observer pattern)
     Game* game = Game::getGame();
     game->initial();
     game->initialPlayer();
     game->playGame();
 
-
-
-
+    //playing game with strategy pattern
+//    Game* game = Game::getGame();
+//    game->initial();
 //    game->initialStrategyPlayer();
 //    game->playGameByStragegy();
 
@@ -27,7 +28,5 @@ int main() {
 
 
 
-
-
-return 0;
+    return 0;
 }
