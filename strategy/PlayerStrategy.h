e8d04815ca9d90e2d50c55ef5e2ce4fea281b9cc
royanz <36062607+royanz@users.noreply.github.com>
2018-04-03 21:46:12 -0400

    Update PlayerStrategy.h
    adding comments

--- strategy/PlayerStrategy.h
+++ strategy/PlayerStrategy.h
@@ -1,12 +1,16 @@
 #pragma once
 #include "../Player.h"
 
+//Strategy Abstract Class
+//The classes that implement a concrete strategy should inherit this.
+//The Player class uses this to use a concrete strategy.
 class Player;
 class PlayerStrategy {
 public:
 	PlayerStrategy() {}
 	~PlayerStrategy() {}
 
+	//Method whose implementation varies depending on the strategy adopted.
 	virtual void play() = 0;
 	virtual void firstConquestByStrategy(Player*, int)= 0;
 	virtual void picks_raceByStrategy(Player*, ComboList&) = 0;
