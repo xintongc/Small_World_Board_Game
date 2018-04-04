65fc721e594cbb04ad7c6298b87565a06f8ae125
royanz <36062607+royanz@users.noreply.github.com>
2018-04-03 22:26:49 -0400

    Update main.cpp
    adding comments

--- main.cpp
+++ main.cpp
@@ -8,22 +8,18 @@ using namespace std;
 
 int main() {
 
+    //playing game (will show observer pattern)
     Game* game = Game::getGame();
     game->initial();
     game->initialPlayer();
     game->playGame();
 
+    //playing game with strategy pattern
+//    Game* game = Game::getGame();
+//    game->initial();
 //    game->initialStrategyPlayer();
 //    game->playGameByStragegy();
 
-
-//    Player* player = new Player(1);
-//    player->setStrategy(new AggressiveStrategy());
-//    player->firstConquestByStrategy(2);
-
-
-
-//    player->firstConquest(2);
 
 
 
