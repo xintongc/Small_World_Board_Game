70a8d45f5d1448b88dcfc76cf1b0b65841eb7690
royanz <36062607+royanz@users.noreply.github.com>
2018-04-07 15:33:10 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,26 +18,6 @@ int main() {
     game->initialStrategyPlayer();
     game->playGameByStragegy();
 
-//    Player* player = new Player(1);
-//    player->setStrategy(new AggressiveStrategy());
-//    player->firstConquestByStrategy(2);
-
-//    ComboList combo;
-//    Player* player = new Player(1);
-//    player->setStrategy(new ModerateStrategy());
-//    player->picks_raceByStrategy(combo);
-
-
-
-// strategy pattern driver
-//    ComboList combo;
-//    combo.setupCombo();
-//    Player player1(1, new AggressiveStrategy());
-//    Player player2(2, new DefensiveStrategy());
-//    Player player3(3, new ModerateStrategy());
-
-
-
 
 
 
